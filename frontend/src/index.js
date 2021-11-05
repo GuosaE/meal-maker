79fc62ce83f4b279ec8cbf02ad94c329d93392ad
Guosa_Edebiri <gedebiri@gmail.com>
2021-11-05 14:52:23 -0500

    added make new recipe static method to Recipe class

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,4 +19,25 @@ class Recipe {
     }
 
 
+    static makeNewRecipe(event) {
+        event.preventDefault();
+
+        const title = event.target.title.value;
+        const image = event.target.image.value;
+        const instructions = event.target.instructions.value;
+        const ingredients = event.target.ingredients.value;
+        const cuisine = event.target.cuisine.value;
+
+        const newRecipe = {
+            title: title,
+            image: image,
+            ingredients: ingredients,
+            instructions: instructions,
+            cuisine: cuisine
+        };
+    
+        Recipe.postNewRecipe(newRecipe);
+    }
+
+
 }
