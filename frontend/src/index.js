2033afd19d5bbd04e98d09bd0376de207f50bd42
Guosa_Edebiri <gedebiri@gmail.com>
2021-11-05 14:51:37 -0500

    added recipe class and constructor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,22 @@
 const recipesURL = 'http://localhost:3000/recipes';
 const recipeContainer = document.querySelector('tbody');
 const form = document.getElementById('add-new-recipe-form')
+
+Recipe.getAllRecipes();
+
+form.addEventListener('submit', Recipe.makeNewRecipe);
+recipeContainer.addEventListener('click', Recipe.deleteExistingRecipe)
+
+class Recipe {
+
+    constructor(id, title, image, instructions, ingredients, cuisine) {
+        this.id = id
+        this.title = title
+        this.image = image
+        this.instructions = instructions
+        this.ingredients = ingredients
+        this.cuisine = cuisine
+    }
+
+
+}
